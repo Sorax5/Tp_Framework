c28db91fe808dade3476e35632731e65f86d8204
SoraxDubbing <sorax5.r23@gmail.com>
2023-09-04 10:33:43 +0200

    FlatManager.ts complete

--- logic/FlatManager.ts
+++ logic/FlatManager.ts
@@ -1,6 +1,66 @@
+import {FlatDao} from "./FlatDao";
+import {Flat} from "./Flat";
+
 /**
  * Manages the flat {@link Flat}
  */
 class FlatManager{
 
+    private flatDao : FlatDao;
+
+    /**
+     * Constructor of the class FlatManager.
+     * @param dao Data access object of the flat.
+     * @constructor
+     */
+    public FlatManager(dao : FlatDao){
+        this.flatDao = dao;
+    }
+
+    /**
+     * Creates a flat.
+     * @param flat Flat to create.
+     */
+    public createFlat(flat : Flat) : void{
+        this.flatDao.createFlat(flat);
+    }
+
+    /**
+     * Reads a flat.
+     * @param id Identifier of the flat to read.
+     */
+    public readFlat(id : number) : Flat{
+        return this.flatDao.readFlat(id);
+    }
+
+    /**
+     * Updates a flat.
+     * @param flat Flat to update.
+     */
+    public updateFlat(flat : Flat) : void{
+        this.flatDao.updateFlat(flat);
+    }
+
+    /**
+     * Deletes a flat.
+     * @param id Identifier of the flat to delete.
+     */
+    public deleteFlat(id : number) : void{
+        this.flatDao.deleteFlat(id);
+    }
+
+    /**
+     * Reads all flats.
+     */
+    public readAllFlat() : Flat[]{
+        return this.flatDao.readAllFlats();
+    }
+
+    /**
+     * Checks if a flat exists.
+     * @param id Identifier of the flat to check.
+     */
+    public existsFlat(id : number) : boolean{
+        return this.flatDao.existsFlat(id);
+    }
 }
